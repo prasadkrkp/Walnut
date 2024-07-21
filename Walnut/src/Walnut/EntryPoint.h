c543bd81b79b4057457f96817071204d1d0d2fcc
Rama Krishna Kandukuri <prasadkrkp@gmail.com>
2024-07-21 17:26:14 +0200

    Linux support for Walnut

---
+++ Walnut/src/Walnut/EntryPoint.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#ifdef WL_PLATFORM_WINDOWS
+    #include <Windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+extern Walnut::Application* Walnut::CreateApplication(int argc, char** argv);
+bool g_ApplicationRunning = true;
+
+namespace Walnut {
+
+    int Main(int argc, char** argv)
+    {
+        while (g_ApplicationRunning)
+        {
+            Walnut::Application* app = Walnut::CreateApplication(argc, argv);
+            app->Run();
+            delete app;
+        }
+
+        return 0;
+    }
+
+}
+
+#ifdef WL_PLATFORM_WINDOWS
+
+#ifdef WL_DIST
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return Walnut::Main(__argc, __argv);
+}
+
+#else
+
+int main(int argc, char** argv)
+{
+    return Walnut::Main(argc, argv);
+}
+
+#endif // WL_DIST
+
+#else // UNIX (Ubuntu)
+
+int main(int argc, char** argv)
+{
+    return Walnut::Main(argc, argv);
+}
+
+#endif // WL_PLATFORM_WINDOWS
---
+++ build/vendor/GLFW/docs/html/search/all_6.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['damage_20and_20refresh_0',['Window damage and refresh',['../window_guide.html#window_refresh',1,'']]],
+  ['deallocate_1',['deallocate',['../structGLFWallocator.html#ab74cf9a969e73e6eb65a6112a591a988',1,'GLFWallocator']]],
+  ['default_20values_2',['default values',['../intro_guide.html#init_hints_values',1,'Supported and default values'],['../window_guide.html#window_hints_values',1,'Supported and default values']]],
+  ['demand_3',['Joystick support is initialized on demand',['../news.html#joysticks_34',1,'']]],
+  ['dependencies_4',['Installing dependencies',['../compile_guide.html#compile_deps',1,'']]],
+  ['dependencies_20for_20wayland_20and_20x11_5',['Dependencies for Wayland and X11',['../compile_guide.html#compile_deps_wayland',1,'']]],
+  ['dependencies_20for_20x11_6',['Dependencies for X11',['../compile_guide.html#compile_deps_x11',1,'']]],
+  ['dependency_20has_20been_20removed_7',['CoreVideo dependency has been removed',['../news.html#corevideo_34',1,'']]],
+  ['deprecated_20list_8',['Deprecated List',['../deprecated.html',1,'']]],
+  ['deprecations_20in_20version_203_204_9',['Deprecations in version 3.4',['../news.html#deprecations_34',1,'']]],
+  ['destruction_10',['destruction',['../input_guide.html#cursor_destruction',1,'Cursor destruction'],['../window_guide.html#window_destruction',1,'Window destruction']]],
+  ['disabled_20when_20built_20as_20a_20sub_20project_11',['Tests and examples are disabled when built as a sub-project',['../news.html#standalone_34',1,'']]],
+  ['drop_20input_12',['Path drop input',['../input_guide.html#path_drop',1,'']]],
+  ['dwm_20transparency_13',['Framebuffer transparency requires DWM transparency',['../news.html#caveat_fbtransparency_34',1,'']]]
+];
